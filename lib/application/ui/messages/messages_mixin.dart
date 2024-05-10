99c38d9c34fd00829436e373209e151fd7749e9e
pollyyana <pollyana.medeirosk@gmail.com>
2024-05-10 00:22:17 -0300

    messages

---
+++ lib/application/ui/messages/messages_mixin.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+mixin MessagesMixin on GetxController {
+  //rxn aceita nulo e  vai ficar escultando as alteraões do MessageModel
+  void messagesListener(Rxn<MessagesModel> message) {
+    //implementação
+    //ever vai executar uma funçao
+
+    ever<MessagesModel?>(message, (model) {
+      //model diferente de null pq ele pode ser passado nulo
+      if (model != null) {
+        Get.snackbar(
+          model.title,
+          model.message,
+          backgroundColor:
+              // (model.type == MessageType.error ? Colors.red : Colors.blue)
+              model.type.color(),
+        );
+      } else {}
+    });
+  }
+}
+
+class MessagesModel {
+  final String title;
+  final String message;
+  final MessageType type;
+
+  MessagesModel({
+    required this.title,
+    required this.message,
+    required this.type,
+  });
+  MessagesModel.error({
+    required this.title,
+    required this.message,
+  }) : type = MessageType.error;
+  MessagesModel.info({
+    required this.title,
+    required this.message,
+  }) : type = MessageType.info;
+}
+
+enum MessageType { error, info }
+
+extension MessageTypeExtension on MessageType {
+  Color color() {
+    // Switch (MessageType){}
+    // ou
+    switch (this) {
+      case MessageType.error:
+        //red 800 nao existir entao retorna nulo se for nulo pode retornar o red padrao
+        return Colors.red[600] ?? Colors.red;
+      case MessageType.info:
+        return Colors.blue[200] ?? Colors.blue;
+    }
+  }
+}
--- lib/modules/login/login_controller.dart
+++ lib/modules/login/login_controller.dart
@@ -1,13 +1,17 @@
 import 'package:app_filmes/application/ui/loader/loader_mixin.dart';
+import 'package:app_filmes/application/ui/messages/messages_mixin.dart';
 import 'package:get/get.dart';
-class LoginController extends GetxController with LoaderMixin{
 
-  final  loading = false.obs;
+class LoginController extends GetxController with LoaderMixin, MessagesMixin {
+  final loading = false.obs;
+  final message = Rxn<MessagesModel>();
 
   @override
+  //esculta as paradas
   void onInit() {
     super.onInit();
     loaderListener(loading);
+    messagesListener(message);
   }
 
   Future<void> login() async {
@@ -16,9 +20,18 @@ class LoginController extends GetxController with LoaderMixin{
     loading(true);
     //Get
     await 2.seconds.delay();
+    //dart
     // await Future.delayed(Duration(seconds: 2));
-
     loading(false);
+    message(MessagesModel.error(
+      title: 'Titulo Error',
+      message: 'Mensagem de erro',
+    ));
+    await 1.seconds.delay();
+
+    message(MessagesModel.info(
+      title: 'Titulo Info',
+      message: 'Mensagem de Info',
+    ));
   }
-  
 }
