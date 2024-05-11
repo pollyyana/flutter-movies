14087483727ae9d2aeeedb03c38fee23556b51ca
pollyyana <pollyana.medeirosk@gmail.com>
2024-05-11 01:48:53 -0300

    icons bottomNvigation..

--- lib/modules/home/home_page.dart
+++ lib/modules/home/home_page.dart
@@ -1,5 +1,8 @@
+import 'package:app_filmes/application/ui/filmes_app_icons_icons.dart';
 import 'package:flutter/material.dart';
-    
+
+//responsavel por carregas a tela movies e favoritos
+  
 class HomePage extends StatelessWidget {
 
   const HomePage({ super.key });
@@ -7,9 +10,11 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Home Page'),
-      ),
+      bottomNavigationBar: BottomNavigationBar(items: const [
+        BottomNavigationBarItem(icon: Icon(Icons.movie), label: 'Filmes'),
+        BottomNavigationBarItem(icon: Icon(FilmesAppIcons.heart_empty), label: 'Favorites', backgroundColor: Colors.black),
+        BottomNavigationBarItem(icon: Icon(Icons.login_outlined), label: 'Sair', backgroundColor: Colors.black),
+      ],),
       body: Container(),
     );
   }
